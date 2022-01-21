cece621cff5c5fe30eae17ac8e6e0ed7bc59a2a3
Sarah <s.keating@ucl.ac.uk>
2022-01-21 14:56:15 +0000

    sorting age

--- anonymising_data/age.py
+++ anonymising_data/age.py
@@ -37,7 +37,6 @@ class Age:
         """
         return self.months
 
-
     def get_years(self):
         """
         Funcion to return age in years
@@ -50,22 +49,44 @@ class Age:
         Function to calculate age from date of birth
         """
         if not testdate:
-            today = date.today()
+            today = datetime.today()
         else:
             today = datetime.strptime(testdate, '%Y-%m-%d')
+
         # work out whether we have had a birthday this year
         # i.e. your dob is xxx-07-27 and today is xxx-02-16 you have not reached the 2nd month or the 27th
         reached_month_of_birthday = (today.month >= self.dob.month)
+        birthday_this_month = today.month == self.dob.month
         reached_day_of_birthday = (today.day >= self.dob.day)
-        had_birthday_this_year = reached_month_of_birthday and reached_day_of_birthday
+        had_birthday_this_year = reached_day_of_birthday if birthday_this_month else reached_month_of_birthday
 
         self.years = today.year - self.dob.year - (not had_birthday_this_year)
 
         if had_birthday_this_year:
-            self.months = today.month - self.dob.month
+            if reached_day_of_birthday:
+                self.days = today.day - self.dob.day
+                self.months = today.month - self.dob.month
+            else:
+                self.days = self._end_of_last_month(today.month - 1, today.year, self.dob.day) + today.day
+                self.months = today.month - self.dob.month -1
         else:
-            self.months = 12 + today.month - self.dob.month
-        self.days = today.day - self.dob.day
+            self.months = 11 + today.month - self.dob.month
+            if reached_day_of_birthday:
+                self.months = 12 + today.month - self.dob.month
+                self.days = today.day - self.dob.day
+            else:
+                self.months = 11 + today.month - self.dob.month
+                self.days = self._end_of_last_month(today.month - 1, today.year, self.dob.day) + today.day
+
+    def _end_of_last_month(self, month, year, day):
+        days_in_months = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+        days = days_in_months[month-1] - day
+        if self._is_leap_year(year) and month == 2:
+            days = days + 1
+        return days
+
+    def _is_leap_year(self, year):
+        return (year % 4) == 0
 
     def calculate_age_for_testing(self, strdate):
         self.__calculate_age(strdate)
