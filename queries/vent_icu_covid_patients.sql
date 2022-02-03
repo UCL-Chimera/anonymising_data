d6351a84a91ebb3b496287eb2b4ba4ae446f28ac
Sarah <s.keating@ucl.ac.uk>
2022-02-03 15:40:30 +0000

    runs but no results

--- queries/vent_icu_covid_patients.sql
+++ queries/vent_icu_covid_patients.sql
@@ -1,39 +1,10 @@
 SELECT DISTINCT
 
-  inf.mrn_id
-    FROM star.patient_condition inf
+  live_mrn.mrn mrn
 
-  -- has covid 
+ FROM star.hospital_visit hv
 
-  JOIN star.condition_type condtype
-
-    ON condtype.condition_type_id = inf.condition_type_id
-  
-    AND condtype.internal_code like '%COVID-19'
-
-  -- were in ICU
-
-  JOIN star.hospital_visit hv
-
-    ON hv.mrn_id = inf.mrn_id
-
-  JOIN star.location_visit lv
-
-    ON lv.hospital_visit_id = hv.hospital_visit_id
-
-  JOIN star.location loc
-
-    ON loc.location_id = lv.location_id
-
-  JOIN star.department dept
-
-    ON loc.department_id = dept.department_id
-
-    AND dept.speciality like '%ICU'
-
- -- make sure current live mrn
-
--- original MRN that may or may not be live  
+  -- original MRN that may or may not be live  
 
 INNER JOIN  star.mrn original_mrn  
 
@@ -51,15 +22,44 @@ INNER JOIN star.mrn live_mrn
 
   ON mtl.live_mrn_id = live_mrn.mrn_id  
 
+  -- has covid 
+  JOIN star.patient_condition inf
+
+  ON inf.hospital_visit_id = hv.hospital_visit_id
+
+  JOIN star.condition_type condtype
+
+    ON condtype.condition_type_id = inf.condition_type_id
+  
+    AND condtype.internal_code like '%COVID-19'
+
+  -- were in ICU
+
+
+  JOIN star.location_visit lv
+
+    ON lv.hospital_visit_id = hv.hospital_visit_id
+
+  JOIN star.location loc
+
+    ON loc.location_id = lv.location_id
+
+  JOIN star.department dept
+
+    ON loc.department_id = dept.department_id
+
+    AND dept.speciality like '%ICU'
+
+
 
 -- was ventilated
 
---JOIN star.visit_observation vo
+JOIN star.visit_observation vo
 
---  ON hv.hospital_visit_id = vo.hospital_visit_id
+  ON hv.hospital_visit_id = vo.hospital_visit_id
 
---INNER JOIN star.visit_observation_type AS vot  
+INNER JOIN star.visit_observation_type AS vot  
 
---  ON vo.visit_observation_type_id = vot.visit_observation_type_id  
+  ON vo.visit_observation_type_id = vot.visit_observation_type_id  
 
---  AND vot.id_in_application = '3040102607'  
+  AND vot.id_in_application = '3040102607'  
