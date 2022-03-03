2668df124e4ffe85df322737babf6a3dfc7cb5bd
Sarah <s.keating@ucl.ac.uk>
2022-03-03 09:17:51 +0000

    more tests

--- anonymising_data/tests/test_recorded_dates.py
+++ anonymising_data/tests/test_recorded_dates.py
@@ -5,6 +5,9 @@ from anonymising_data.recorded_date import RecordedDate
 
 @pytest.mark.parametrize("testdate, offset, shifted", [
     ('2000-02-10', 367, '2001-02-11'),
+    ('1999-02-10', 367, '2000-02-12'),
+    ('2000-03-10', 53, '2000-05-02'),
+    ('2000-03-10', -10, '2000-02-29'),
 ])
 def test_shift_date(testdate, offset, shifted):
     """
