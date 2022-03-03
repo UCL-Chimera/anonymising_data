32599bcf696e08214163e5e6989936b3ee4751f5
Sarah <s.keating@ucl.ac.uk>
2022-03-03 09:08:46 +0000

    first pass at shifting data

---
+++ anonymising_data/recorded_date.py
@@ -0,0 +1,70 @@
+from datetime import datetime, timedelta
+
+
+class RecordedDate:
+    """
+    Class to shift the date and time by prescribed amount
+    """
+    def __init__(self, original):
+        self.original = original
+        self.offset = 0
+        self.shifted_date = original
+
+    @property
+    def original(self):
+        """
+        Function to retrieve the original date
+        :return: _original_date
+        """
+        return self._original
+
+    @original.setter
+    def original(self, value):
+        """
+        Function to set the original date
+        """
+        self._original = datetime.strptime(value, '%Y-%m-%d')
+        print('ss')
+
+    @property
+    def shifted_date(self):
+        """
+        Function to retrieve the shifted date
+        :return: _shifted_date
+        """
+        return self._shifted_date
+
+    @shifted_date.setter
+    def shifted_date(self, value):
+        """
+        Function to set the shifted date
+        """
+        self._shifted_date = datetime.strptime(value, '%Y-%m-%d')
+
+    @property
+    def offset(self):
+        """
+        Function to retrieve the offset period
+        :return: _offset
+        """
+        return self._offset
+
+    @offset.setter
+    def offset(self, value):
+        """
+        Function to set the offset period in days
+        """
+        self._offset = value
+
+    def shift_date(self):
+        """
+        Function to shift date by the offset period
+        """
+        if not self._offset:
+            self._shifted_date = self._original
+        else:
+            self._shifted_date = self._original + timedelta(days=self._offset)
+
+
+
+
---
+++ anonymising_data/tests/test_recorded_dates.py
@@ -0,0 +1,19 @@
+import pytest
+from datetime import datetime
+from anonymising_data.recorded_date import RecordedDate
+
+
+@pytest.mark.parametrize("testdate, offset_date, shifted", [
+    ('2000-02-10', 367, '2001-02-11'),
+])
+def test_shift_date(testdate, offset_date, shifted):
+    """
+    Test to check shift date function works as expected
+    :param testdate: original date
+    :param offset: period to shift
+    :param shifted: shifted date
+    :return:    """
+    start = RecordedDate(testdate)
+    start.offset = offset_date
+    start.shift_date()
+    assert(start.shifted_date == datetime.strptime(shifted, '%Y-%m-%d'))
