3e3bb1157ffe218ca9c24981f3cee41ba773e964
Sarah <s.keating@ucl.ac.uk>
2023-02-09 11:18:56 +0000

    adding omop queries

---
+++ queries/omop-es/get_concept_id.sql
@@ -0,0 +1,6 @@
+SELECT * FROM hic_cc_002.concept AS m
+
+WHERE
+m.domain_id LIKE '%Meas%'
+AND 
+m.concept_name LIKE '%pH%'
---
+++ queries/omop-es/get_data.sql
@@ -0,0 +1,25 @@
+
+SELECT c.concept_name AS measurement_type,
+       m.person_id,
+	   v.visit_occurrence_id AS visit,
+       m.measurement_datetime,
+	   m.value_as_number,
+	   2023 - p.year_of_birth AS age,
+	   p.gender_source_value AS Gender,
+	   p.race_source_value AS Ethnicity
+FROM hic_cc_002.measurement AS m
+
+INNER JOIN hic_cc_002.person AS p
+ON m.person_id = p.person_id
+
+INNER JOIN hic_cc_002.concept AS c
+ON c.concept_id = m.measurement_concept_id
+
+INNER JOIN hic_cc_002.visit_occurrence AS v
+ON v.visit_occurrence_id = m.visit_occurrence_id
+WHERE 
+m.measurement_concept_id = 4302666  -- TEMPERATURE
+OR
+m.measurement_concept_id = 3010421 -- pH
+ORDER BY m.person_id 
+--LIMIT 10
