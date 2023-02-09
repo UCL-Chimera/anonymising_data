e9b0686111675aad14f12c992d91dacc0de2887a
Sarah <s.keating@ucl.ac.uk>
2023-02-09 14:23:23 +0000

    get value for a result as string

--- queries/omop-es/get_data.sql
+++ queries/omop-es/get_data.sql
@@ -4,6 +4,13 @@ SELECT c.concept_name AS measurement_type,
 	   v.visit_occurrence_id AS visit,
        m.measurement_datetime,
 	   m.value_as_number,
+       -- if value is not a number it lists the concept_id
+       -- look up name if there is a value
+       	(
+		   SELECT cc.concept_name FROM hic_cc_002.concept AS cc
+		   WHERE cc.concept_id = m.value_as_concept_id
+		   AND cc.concept_name NOT LIKE 'No matching concept'
+	    ) AS value_as_string ,
 	   2023 - p.year_of_birth AS age,
 	   p.gender_source_value AS Gender,
 	   p.race_source_value AS Ethnicity
