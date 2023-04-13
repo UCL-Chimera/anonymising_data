357826ca57d3f55af95dae80b281ddfc03c0db6f
Sarah <s.keating@ucl.ac.uk>
2023-04-13 09:06:41 +0100

    more tests

--- anonymising_data/tests/test_utils.py
+++ anonymising_data/tests/test_utils.py
@@ -1,5 +1,5 @@
 from anonymising_data.utils.helpers import determine_date_format,\
-    has_expected_date_format
+    has_expected_date_format, rreplace
 
 import pytest
 
@@ -9,7 +9,9 @@ import pytest
     ('1999-02-10', True),
     ('2023/08/10', True),
     ('13-09-4321', True),
-    ('21/12/3090', True)
+    ('21/12/3090', True),
+    ('2000-02/10', False),
+
 ])
 def test_has_expected_format(date_str, correct_format):
     assert (has_expected_date_format(date_str) is correct_format)
@@ -23,3 +25,23 @@ def test_has_expected_format(date_str, correct_format):
 ])
 def test_get_date_format(date_str, correct_format):
     assert (determine_date_format(date_str) == correct_format)
+
+
+@pytest.mark.parametrize("original, old, new, occurrences, produced", [
+    ('test1', 't', 'W', 1, 'tesW1'),
+    ('test1', 't', 'W', 2, 'WesW1'),
+    ('test1', 't', 'W', 3, 'WesW1'),
+    ('test1', 'a', 'W', 1, 'test1'),
+    ('this is a test it is', 'is', 'REP', 1, 'this is a test it REP'),
+    ('this is a test it is', 'is', 'REP', 3, 'thREP REP a test it REP'),
+])
+def test_rreplace(original, old, new, occurrences, produced):
+    """
+    Function to test rreplace function
+    :param original: original string
+    :param old: string to be replaced
+    :param new: string replacement
+    :param occurrences: no of occurences to replace
+    :param produced: result string
+    """
+    assert (rreplace(original, old, new, occurrences) == produced)
