59a31b4a4ac1086b1423e9570519da9f62198d3d
Sarah <s.keating@ucl.ac.uk>
2023-04-28 12:48:18 +0100

    add source to concept file

--- anonymising_data/tests/test_get_concepts.py
+++ anonymising_data/tests/test_get_concepts.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 
 from anonymising_data.retrieve_data.get_concepts import Concepts
-from anonymising_data.retrieve_data.get_concepts import get_concept_id
+from anonymising_data.retrieve_data.get_concepts import get_concept_id_and_source
 
 import pytest
 
@@ -17,17 +17,17 @@ def test_create_concepts(concept_file):
             joinpath('tests/resources/test_concept_codes.csv'))
 
 
-@pytest.mark.parametrize("line, concept_id", [
-    ('400, SNOMED, 432, Body temperature, Measurement', '432'),
-    ('12 - Jul, LOINC, 543, Oxygen[Partial pressure], Measurement', '543'),
+@pytest.mark.parametrize("line, concept_id, source", [
+    ('400, SNOMED, 432, Body temperature, S1, Measurement', '432', 'S1'),
+    ('12 - Jul, LOINC, 543, Oxygen[Partial pressure], S2, Measurement', '543', 'S2'),
 ])
-def test_get_concept(line, concept_id):
+def test_get_concept(line, concept_id, source):
     """
     Function to test reading the concept ID
     :param line: The line read from the results of the query file.
     :param concept_id: The expected concept ID
     """
-    assert (get_concept_id(line) == concept_id)
+    assert (get_concept_id_and_source(line) == [concept_id, source])
 
 
 def test_get_concepts(concept_file):
@@ -39,3 +39,17 @@ def test_get_concepts(concept_file):
     con = Concepts(concept_file)
     con.populate_concepts()
     assert (con.concepts == ['1', '23', '5872'])
+
+
+def test_get_sources(concept_file):
+    """
+    Function to test the populate_concepts function
+    returns dictionary of ids and sources
+    :param concept_file: Concepts from Pytest fixtures
+    :return:
+    """
+    con = Concepts(concept_file)
+    con.populate_concepts()
+    assert (con.source == {'1': 'TEMPERATURE',
+                           '23': 'AQURE TEMPERATURE CORRECTED OXYGEN',
+                           '5872': 'Source'})
