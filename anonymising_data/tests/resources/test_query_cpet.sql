81d5edc816b3f24baebfea13e403ff8ee903d4d0
Sarah <s.keating@ucl.ac.uk>
2024-01-28 11:27:55 +0000

    add files for testing

---
+++ anonymising_data/tests/resources/test_query_cpet.sql
@@ -0,0 +1,28 @@
+SELECT
+    c.concept_name AS measurement_type,
+    c.concept_id AS measurement_source,
+    m.person_id,
+    m.measurement_datetime,
+    m.value_as_number,
+	m.unit_source_value AS units,
+    (
+        SELECT cc.concept_name FROM :FILL_SCHEMA:concept AS cc
+        WHERE cc.concept_id = m.value_as_concept_id
+            AND cc.concept_name NOT LIKE 'No matching concept'
+    ) AS value_as_string,
+    p.birth_datetime AS age,
+    p.gender_source_value AS gender,
+    p.race_source_value AS ethnicity
+FROM :FILL_SCHEMA:measurement AS m
+
+INNER JOIN :FILL_SCHEMA:person AS p
+    ON m.person_id = p.person_id
+
+INNER JOIN :FILL_SCHEMA:concept AS c
+    ON c.concept_id = m.measurement_concept_id
+
+WHERE
+
+    m.measurement_concept_id IN :FILL_CONCEPT:
+
+ORDER BY m.person_id
---
+++ anonymising_data/tests/resources/test_query_expected_cpet.sql
@@ -0,0 +1,28 @@
+SELECT
+    c.concept_name AS measurement_type,
+    c.concept_id AS measurement_source,
+    m.person_id,
+    m.measurement_datetime,
+    m.value_as_number,
+	m.unit_source_value AS units,
+    (
+        SELECT cc.concept_name FROM concept AS cc
+        WHERE cc.concept_id = m.value_as_concept_id
+            AND cc.concept_name NOT LIKE 'No matching concept'
+    ) AS value_as_string,
+    p.birth_datetime AS age,
+    p.gender_source_value AS gender,
+    p.race_source_value AS ethnicity
+FROM measurement AS m
+
+INNER JOIN person AS p
+    ON m.person_id = p.person_id
+
+INNER JOIN concept AS c
+    ON c.concept_id = m.measurement_concept_id
+
+WHERE
+
+    m.measurement_concept_id IN (1, 23, 5872)
+
+ORDER BY m.person_id
