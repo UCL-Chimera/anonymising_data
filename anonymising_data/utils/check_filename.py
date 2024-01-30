ecc27e6b9f49ee357c16cd87cb1864382428b7bb
nikk-nikaznan <nik.k.nikaznan@outlook.com>
2024-01-30 13:26:07 +0000

    changing check_filename function so it works on the real data

--- anonymising_data/utils/check_filename.py
+++ anonymising_data/utils/check_filename.py
@@ -2,10 +2,10 @@ import re
 
 
 def extract_and_check_format(xml_filename):
-    match = re.match(r"^CPET\d{2,4}", xml_filename)
+    match = re.search(r"([^_]*CPET\d{2,4})", xml_filename)
 
     if match:
-        extracted_value = match.group()
+        extracted_value = match.group(1)
         return extracted_value
     else:
         return xml_filename
