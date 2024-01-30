e4bde462e6bd2735ea0bc59fded6e90c2e6b1ab0
nikk-nikaznan <nik.k.nikaznan@outlook.com>
2024-01-30 09:57:39 +0000

    another test

--- anonymising_data/tests/test_mapping.py
+++ anonymising_data/tests/test_mapping.py
@@ -1,5 +1,8 @@
 import pytest
 import os
+import tempfile
+from pathlib import Path
+import pandas as pd
 
 from anonymising_data.retrieve_data.final_output_xml import Data
 from anonymising_data.retrieve_data.retrieve_xml import RetrieveXML
@@ -71,13 +74,42 @@ def test_person_id_list(instance, xml_directory):
     assert set(expected_person_id) == set(person_id_list)
 
 
-def preprocess_list(input_list):
-    """Convert 'na' and empty cells to a common representation in a list of strings."""
-    processed_rows = []
-    for row in input_list:
-        elements = row.split(",")
-        processed_row = [
-            cell.strip() if cell.strip() != "" else "na" for cell in elements
-        ]
-        processed_rows.append(processed_row)
-    return processed_rows
+def test_write_demographic_output(instance, xml_directory):
+    """
+    Functions to test the list of person_id.
+    :param xml_config: Configuration class from Pytest fixtures
+    """
+    retriever, final_output = instance
+    file_path = Path(__file__).parent.parent.joinpath(
+        "tests/resources/CPet/expected-input/CPET0818-TONY-JONES.xml"
+    )
+
+    with tempfile.NamedTemporaryFile(mode="w+", delete=False) as temp_file:
+        temp_output = temp_file.name
+        csv_files = retriever.get_data(file_path)
+
+        with open(csv_files, "r") as output_file:
+            output_csv_content = output_file.readlines()
+
+            final_output._create_demographic_output(
+                output_csv_content, temp_output
+            )
+        demographic_output_df = pd.read_csv(temp_output)
+
+        expected_file = Path(__file__).parent.parent.joinpath(
+            "tests/resources/CPet/expected-output/cpet_demographics_report_data.csv"
+        )
+        demographic_expected_df = pd.read_csv(expected_file)
+
+        columns_to_check = ["person_id", "gender", "age", "height", "weight"]
+
+        is_row_in_output_df = (
+            (
+                demographic_output_df[columns_to_check]
+                == demographic_expected_df.iloc[1][columns_to_check]
+            )
+            .all(axis=1)
+            .any()
+        )
+
+        assert is_row_in_output_df
