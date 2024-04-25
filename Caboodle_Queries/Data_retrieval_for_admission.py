7cab18b66b720a1834e4c52f2ae278697f9fea16
Zakaria Senousy <160079149+zsenousyUCL@users.noreply.github.com>
2024-04-25 11:33:30 +0100

    Update Data_retrieval_for_admission.py

--- Caboodle_Queries/Data_retrieval_for_admission.py
+++ Caboodle_Queries/Data_retrieval_for_admission.py
@@ -1,17 +1,16 @@
 SELECT
-    dbo.HospitalAdmissionFact.HospitalAdmissionKey,
-	  dbo.PatientDim.PatientKey,
-    dbo.PatientDim.GenderIdentity,
-  	dbo.PatientDim.Sex,
-  	dbo.PatientDim.Ethnicity,
-  	dbo.PatientDim.BirthDate,
-  	dbo.HospitalAdmissionFact._CreationInstant,
-  	dbo.HospitalAdmissionFact.DischargeInstant,
-  	dbo.HospitalAdmissionFact.AdmissionSource,
-  	dbo.HospitalAdmissionFact.AdmissionType,
-  	dbo.HospitalAdmissionFact.DischargeDisposition,
-  	dbo.HospitalAdmissionFact.DischargeDestination_X
-
+	dbo.HospitalAdmissionFact.HospitalAdmissionKey,
+	dbo.PatientDim.PatientKey,
+	dbo.PatientDim.GenderIdentity,
+	dbo.PatientDim.Sex,
+	dbo.PatientDim.Ethnicity,
+	dbo.PatientDim.BirthDate,
+	dbo.HospitalAdmissionFact._CreationInstant,
+	dbo.HospitalAdmissionFact.DischargeInstant,
+	dbo.HospitalAdmissionFact.AdmissionSource,
+	dbo.HospitalAdmissionFact.AdmissionType,
+	dbo.HospitalAdmissionFact.DischargeDisposition,
+	dbo.HospitalAdmissionFact.DischargeDestination_X
 FROM
     dbo.PatientDim
     INNER JOIN dbo.HospitalAdmissionFact ON dbo.PatientDim.PatientKey = dbo.HospitalAdmissionFact.PatientKey;
@@ -21,9 +20,9 @@ FROM
 
 
 SELECT
-    dbo.VisitFact.VisitKey,
-	  dbo.PatientDim.PatientKey,
-    dbo.PatientDim.GenderIdentity,
+	dbo.VisitFact.VisitKey,
+	dbo.PatientDim.PatientKey,
+	dbo.PatientDim.GenderIdentity,
   	dbo.PatientDim.Sex,
   	dbo.PatientDim.Ethnicity,
   	dbo.PatientDim.BirthDate,
@@ -31,7 +30,6 @@ SELECT
   	dbo.VisitFact._LastUpdatedInstant,
   	dbo.VisitFact.SchedulingSource,
   	dbo.VisitFact.VisitType
-    
 FROM
     dbo.VisitFact
     INNER JOIN dbo.PatientDim ON dbo.VisitFact.PatientKey = dbo.PatientDim.PatientKey;
